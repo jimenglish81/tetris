86acd2e485b3a2f2846422b4593935f183e1a869
jenglish81 <jimenglish81@gmail.com>
2014-05-05 19:35:39 +0100

    tidy up

--- scripts/block.js
+++ scripts/block.js
@@ -12,19 +12,19 @@ Block.prototype.blocks = null;
 
 Block.rotate = function(block) {
 	var originalHeight = block.blocks.length;
-    var rotatedBlocks = Block.rotateBlocks(block.blocks);
-    var height = rotatedBlocks.length;
+   var rotatedBlocks = Block.rotateBlocks(block.blocks);
+   var height = rotatedBlocks.length;
 
-    return new Block(rotatedBlocks, block.x, block.y - height + originalHeight);	
+   return new Block(rotatedBlocks, block.x, block.y - height + originalHeight);	
 };
 
 Block.rotateBlocks = function(blocks) {
 	return blocks.reduce(function(result, row, i, rows){
 		for (var j = 0, l = row.length; j < l; j++) {
 			if (!result[l-1-j]) {
-				result[l-1-j] = [];
+			   result[l-1-j] = [];
 			}
-            result[l-1-j][i] = rows[i][j];	
+         result[l-1-j][i] = rows[i][j];	
 		}
 		return result;
 	}, []);
@@ -32,7 +32,7 @@ Block.rotateBlocks = function(blocks) {
 
 Block.build = function(x, y, index) {
 	var blockTypes = Block.TYPES;
-    return new Block(blockTypes[index || Math.floor(Math.random() * blockTypes.length)], x, y);
+   return new Block(blockTypes[index || Math.floor(Math.random() * blockTypes.length)], x, y);
 };
 
 Block.WIDTH = 4;
@@ -40,28 +40,28 @@ Block.WIDTH = 4;
 Block.HEIGHT = 3;
 
 Block.TYPES = [
-    [[0,0,0,0],
-     [1,1,1,1],
-     [0,0,0,0]],
+   [[0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0]],
 
-    [[0,0,0],
-     [2,2,2],
-     [0,0,2]],
+   [[0,0,0],
+    [2,2,2],
+    [0,0,2]],
 
-    [[0,0,0],
-     [3,3,3],
-     [3,0,0]],
+   [[0,0,0],
+    [3,3,3],
+    [3,0,0]],
 
-    [[4,4],
-     [4,4]],
+   [[4,4],
+    [4,4]],
 
-    [[0,5,5],
-     [5,5,0]],
+   [[0,5,5],
+    [5,5,0]],
 
-    [[0,0,0],
-     [6,6,6],
-     [0,6,0]],
+   [[0,0,0],
+    [6,6,6],
+    [0,6,0]],
 
-    [[7,7,0],
-     [0,7,7]],
+   [[7,7,0],
+    [0,7,7]],
 ];
